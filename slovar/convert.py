bd5399d863549cd1646fa11ad6059326cc867680
Nicolas Cadou <ncadou@cadou.ca>
2018-04-13 13:52:49 -0400

    * eliminate prf dependency by bringing dictset here

---
+++ slovar/convert.py
@@ -0,0 +1,179 @@
+import six
+# from prf.utils.utils import split_strip, str2dt, qs2dict
+from slovar.strings import split_strip
+# from prf.utils.errors import DKeyError,DValueError
+
+
+def parametrize(func):
+
+    def wrapper(dset, name, default=None, raise_on_empty=False, pop=False,
+                            allow_missing=False, set_as=None, pop_empty=False,
+                            _raise=True, **kw):
+
+        if pop_empty:
+            allow_missing=True
+
+        if default is None:
+            try:
+                value = dset[name]
+            except KeyError:
+                if not allow_missing:
+                    raise KeyError("Missing '%s'" % name)
+                else:
+                    return
+        else:
+            value = dset.get(name, default)
+
+        if pop_empty and not value:
+            dset.pop(name, None)
+            return
+
+        if raise_on_empty and not value:
+            raise ValueError("'%s' can not be empty" % name)
+
+        try:
+            result = func(dset, value, **kw)
+        except:
+            if _raise:
+                import sys
+                raise ValueError(sys.exc_info()[1])
+            else:
+                if default is None:
+                    return
+                else:
+                    result = default
+
+        if pop:
+            dset.pop(name, None)
+            if set_as:
+                dset[set_as] = result
+        else:
+            dset[set_as or name] = result
+
+        return result
+
+    return wrapper
+
+
+@parametrize
+def asbool(dset, value):
+    truthy = frozenset(('t', 'true', 'y', 'yes', 'on', '1'))
+    falsey = frozenset(('f', 'false', 'n', 'no', 'off', '0'))
+
+    if value is None:
+        return False
+
+    if isinstance(value, bool):
+        return value
+
+    lvalue = str(value).strip().lower()
+    if lvalue in truthy:
+        return True
+    elif lvalue in falsey:
+        return False
+    else:
+        raise ValueError('Dont know how to convert `%s` to bool' % value)
+
+
+@parametrize
+def aslist(dset, value, sep=',', remove_empty=True, unique=False):
+    if isinstance(value, list):
+        _lst = value
+    elif isinstance(value, str):
+        _lst = split_strip(value, sep, remove_empty)
+    else:
+        _lst = [value]
+
+    if remove_empty:
+        _lst = (list(filter(bool, _lst)))
+
+    if unique:
+        _lst = list(set(_lst))
+
+    return _lst
+
+
+def asset(*args, **kw):
+    return set(aslist(*args, **kw))
+
+
+@parametrize
+def asint(dset, value):
+    return int(value)
+
+
+@parametrize
+def asfloat(dset, value):
+    return float(value)
+
+
+@parametrize
+def asstr(dset, value):
+    return str(value)
+
+
+@parametrize
+def asunicode(dset, value):
+    return str(value)
+
+
+@parametrize
+def asrange(dset, value, typecast=str, sep='-'):
+    if isinstance(value, list):
+        list_ = value
+    elif isinstance(value, str):
+        rng = split_strip(value, sep)
+        if len(rng) !=2:
+            raise ValueError('bad range')
+        list_ = list(range(int(rng[0]), int(rng[1])+1))
+
+    else:
+        list_ = [value]
+
+    return [typecast(e) for e in list_]
+
+
+def asdict(dset, name, _type=None, _set=False, pop=False):
+    """
+    Turn this 'a:2,b:blabla,c:True,a:'d' to {a:[2, 'd'], b:'blabla', c:True}
+
+    """
+
+    try:
+        value = dset[name]
+    except KeyError:
+        raise KeyError("Missing '%s'" % name)
+
+    if _type is None:
+        _type = lambda t: t
+
+    dict_str = dset.pop(name, None)
+    if not dict_str:
+        return {}
+
+    _dict = {}
+    for item in split_strip(dict_str):
+        key, _, val = item.partition(':')
+        if key in _dict:
+            if type(_dict[key]) is list:
+                _dict[key].append(val)
+            else:
+                _dict[key] = [_dict[key], val]
+        else:
+            _dict[key] = _type(val)
+
+    if _set:
+        dset[name] = _dict
+    elif pop:
+        dset.pop(name, None)
+
+    return _dict
+
+
+# @parametrize
+# def asdt(dset, value):
+#     return str2dt(value)
+
+# @parametrize
+# def asqs(dset, value):
+#     return qs2dict(value)
--- slovar/dictionaries.py
+++ slovar/dictionaries.py
@@ -1,3 +1,73 @@
+# from prf.utils.utils import json_dumps
+# from prf.utils.errors import DKeyError, DValueError
+
+from slovar import slovar
+from slovar.convert import *
+
+
+class dictset(slovar):
+
+    def asbool(self, *arg, **kw):
+        return asbool(self, *arg, **kw)
+
+    def aslist(self, *arg, **kw):
+        return aslist(self, *arg, **kw)
+
+    def asset(self, *arg, **kw):
+        return asset(self, *arg, **kw)
+
+    def asint(self, *arg, **kw):
+        return asint(self, *arg, **kw)
+
+    def asfloat(self, *arg, **kw):
+        return asfloat(self, *arg, **kw)
+
+    def asdict(self, *arg, **kw):
+        return asdict(self, *arg, **kw)
+
+    def asdt(self, *arg, **kw):
+        return asdt(self, *arg, **kw)
+
+    def asstr(self, *arg, **kw):
+        return asstr(self, *arg, **kw)
+
+    def asrange(self, *arg, **kw):
+        return asrange(self, *arg, **kw)
+
+    def asqs(self, *arg, **kw):
+        return asqs(self, *arg, **kw)
+
+    def json(self):
+        return json_dumps(self)
+
+    def __setattr__(self, key, val):
+        if isinstance(val, dict):
+            val = dictset(val)
+        self[key] = val
+
+
+# class dkdict(dictset):
+#     def raise_getattr_exc(self, error):
+#         raise DKeyError(error)
+
+#     def raise_value_exc(self, error):
+#         raise DValueError(error)
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat().split(".")[0]
+        try:
+            return super(JSONEncoder, self).default(obj)
+        except TypeError:
+            return str(obj)  # fallback to unicode
+
+
+def json_dumps(body):
+    return json.dumps(body, cls=JSONEncoder)
+
+
 def _extend_list(_list, length):
     if len(_list) < length:
         for _ in range(length - len(_list)):
