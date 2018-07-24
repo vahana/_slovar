30fc679baa2b3bc4491cd9fcaf06a472d0bf7b42
vahan <vahan@gazelle.company>
2018-07-24 20:36:30 +0000

    * added fuzzy_match and fuzzy_sort

--- slovar/utils.py
+++ slovar/utils.py
@@ -49,3 +49,40 @@ def maybe_dotted(module, throw=True):
         except ImportError as e:
             log.error('%s not found. %s' % (module, e))
 
+
+def fuzzy_match(name1, name2):
+    import jellyfish
+
+    """
+        match_rating_comparision - phonetic comparison. works on 5 chars and more. returns None if < 4 chars
+        jaro_distance - string-edit distance.
+            values are floats between [0-1], where 0 is completely dissimilar strings and 1 is identical
+    """
+
+    n1 = str(name1.upper())
+    n2 = str(name2.upper())
+
+    return dict(
+            mra = jellyfish.match_rating_comparison(n1, n2),
+            jaro_distance = round(jellyfish.jaro_distance(n1, n2), 1)
+        )
+
+def fuzzy_sort(n1, nn, cuttoff=0.8):
+    """
+        will sort nn by the best fuzzy match with n1 using fuzzy_match method in this module.
+    """
+
+    matches = []
+
+    for each in nn:
+        match = fuzzy_match(n1, each)
+        if match['mra'] and match['jaro_distance'] >= cuttoff:
+            matches.append([each, match])
+
+    def _key_func(xx):
+        return xx[1]['jaro_distance']
+
+    if matches:
+        return sorted(matches, key=_key_func, reverse=True)
+
+
